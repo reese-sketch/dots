e30b4e56311bd0911b040b85590118a9776724b2
reese-sketch <RBuchanan1@Student.DallasCollege.edu>
2025-09-14 16:53:46 -0500

    Delete install directory

--- install/hypr_setup.sh
+++
@@ -1,9 +0,0 @@
-echo "Setting up hyprpm."
-hyprpm update
-echo "hyprpm updated"
-hyprpm add https://github.com/hyprwm/hyprland-plugins
-hyprpm enable "borders-plus-plus"
-hyprpm reload
-swww img "$HOME/Pictures/nothing.jpg"
-wal -i "$HOME/Pictures/nothing.jpg"
-hyprctl reload
