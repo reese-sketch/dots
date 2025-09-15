a72ddf73c0f5089380f3e7f99d99c6e2b3c81870
reese-sketch <RBuchanan1@Student.DallasCollege.edu>
2025-09-15 11:41:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,76 @@
-echo "-------------------------------------------------"
-echo " ....  ..... ..... ..... ..... .     ..... ..... "
-echo " .   . .   .   .   .       .   .     .     .     "
-echo " .   . .   .   .   .....   .   .     ..... ..... "
-echo " .   . .   .   .   .       .   .     .         . "
-echo " ....  ....    .   .     ..... ..... ..... ..... "
-echo "-------------------------------------------------"
-echo "Installing git"
-sudo pacman -S git clang meson cpio cmake pkg-config gcc
-cd "$HOME"
-echo "downloading dotfiles"
-git clone "https://github.com/reese-sketch/dots.git"
-cd "$HOME/dots"
-sudo pacman -S ttf-jetbrains-mono-nerd waybar rofi python-pywal swww fastfetch
-echo "dependencies downloaded"
-echo "running pacman -Sy just in case"
-sudo pacman -Sy
-echo "creating directories"
-./"install/create_dir.sh "
-echo "directory creation complete"
-./"install/hypr_setup.sh"
-echo "hypr setup complete"
-./"install/pywal.sh"
-echo "pywal setup complete"
-./"install/move_files.sh"
-echo "files moved"
-echo "Changes complete. press SUPER + Shift + E to exit hyprland and log back in."
+$config_path = "$user/.config";
+
+clear
+echo "dotfiles ^_^" ;
+
+
+echo "checking for yay on local machine";
+
+if [ -x '/bin/yay' ]; then
+
+	echo "yay installed!";
+
+else
+
+	echo "yay missing!";
+
+	echo "installing yay (latest) from the AUR";
+
+	sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si;
+
+fi;
+
+
+	echo "installing dependencies";
+
+	echo "installing peaclock, matugen, kitty, swww, wofi, cava, fastfetch, doas";
+
+	echo "configuring doas is easy. edit /etc/doas.conf with 'sudo vim /etc/doas.conf' and append 'permit nopass :wheel'. you'll never have to use sudo again! (setup is insecure for multi-user, visit https://wiki.archlinux.org/title/Doas#Configuration to properly configure doas)";
+
+	yay -S --noconfirm;
+
+	sudo pacman -S wofi swww kitty hyprland cava --noconfirm;
+
+	yay -S matugen doas peaclock waybar-cava --noconfirm;
+
+
+	echo "installing dots!"
+
+	cp -r ./ $config_dir/;
+
+	echo "removing copied 'install.sh' and 'readme.md'";
+
+	rm $config_dir/install.sh;
+
+	rm $config_dir/readme.md;
+
+	hyprctl reload;
+
+	echo "Setting up wallpapers";
+
+	mkdir $user/Pictures/;
+
+	cp wallpapers/* $user/Pictures/;
+
+	chmod +x $config_dir/hypr/wofi-wallpaper-selector.sh;
+
+	matugen image $user/Pictures/lain2.jpg;
+	
+	hyprctl reload;
+
+	echo "Setting up waybar";
+
+	hyprctl dispatch exec waybar;
+
+	echo "moving .bashrc to $user's directory";
+
+	mv .bashrc $user/.bashrc;
+
+	echo "these dotfiles come with a bash function to access the configurations. it's modular and can be edited easily! open a new terminal and type 'rice'. ^_^"
+
+	sleep 1;
+
+
+	echo "install complete!";
+
+
